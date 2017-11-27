2acb9c57f51c31714c628e1fd270a9ed7486a123
William Leingang <wleingang15@mail.wou.edu>
2017-11-27 12:57:22 -0800

    I get the JSON string back for a set search now.

--- HW7-3/Controllers/HomeController.cs
+++ HW7-3/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace HW7_3.Controllers
@@ -11,6 +12,8 @@ namespace HW7_3.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            string test = WebConfigurationManager.AppSettings["test"];
+            System.Diagnostics.Debug.WriteLine(test);
             return View();
         }
     }
---
+++ HW7-3/Scripts/onceAgain.js
@@ -0,0 +1,29 @@
+﻿
+var httpRequest;
+//document.getElementById("getter").addEventListener('click', requesting);
+
+function requesting() {
+    httpRequest = new XMLHttpRequest();
+
+    if (!httpRequest) {
+        console.log("Oops, something went wrong and I couldn't make the http instance. Sorry.");
+        return false;
+    }
+    httpRequest.onreadystatechange = woop;
+    httpRequest.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=
+       // I cut out the key before I committed. &q=cats&limit=25&offset=0&rating=G&lang=en');
+    httpRequest.send();
+}
+
+function woop() {
+    if (httpRequest.readyState == XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            console.log("I did it! Here it is!");
+            console.log(httpRequest.responseText);
+        }
+        else {
+            console.long("I failed again, sorry. I just can't do anything write anymore.");
+        }
+    }
+}
+
