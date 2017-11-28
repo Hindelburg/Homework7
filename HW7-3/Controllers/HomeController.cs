e75e3f6cdb4b4c50ee78124edcb31cefd356fd34
William Leingang <wleingang15@mail.wou.edu>
2017-11-27 16:57:37 -0800

    I got it to return images. Looks pretty bad for now though.

--- HW7-3/Controllers/HomeController.cs
+++ HW7-3/Controllers/HomeController.cs
@@ -1,20 +1,56 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Configuration;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace HW7_3.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(string search)
         {
-            string test = WebConfigurationManager.AppSettings["test"];
-            System.Diagnostics.Debug.WriteLine(test);
             return View();
+        }
+        public ActionResult Find(string search)
+        {
+            string s = "https://api.giphy.com/v1/gifs/search?api_key=" + System.Web.Configuration.WebConfigurationManager.AppSettings["test"] + "&q=" + search + "&limit=25&offset=0&rating=G&lang=en";
+            Debug.WriteLine(s);
+
+
+            WebRequest request = WebRequest.Create(s);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string response2 = reader.ReadToEnd();
+
+            Debug.WriteLine(response2);
+
+            //JObject result = JObject.Parse(response2);
+
+            //Debug.WriteLine(result);
+
+
+            var test = new JavaScriptSerializer().DeserializeObject(response2);
+
+            Debug.WriteLine(test);
+
+            reader.Close();
+            response.Close();
+
+            JsonResult errorMessage = Json(test, JsonRequestBehavior.AllowGet);
+
+            Debug.WriteLine(errorMessage);
+
+            return Json(test, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- HW7-3/Scripts/onceAgain.js
+++ HW7-3/Scripts/onceAgain.js
@@ -3,27 +3,17 @@ var httpRequest;
 //document.getElementById("getter").addEventListener('click', requesting);
 
 function requesting() {
-    httpRequest = new XMLHttpRequest();
 
-    if (!httpRequest) {
-        console.log("Oops, something went wrong and I couldn't make the http instance. Sorry.");
-        return false;
-    }
-    httpRequest.onreadystatechange = woop;
-    httpRequest.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=
-       // I cut out the key before I committed. &q=cats&limit=25&offset=0&rating=G&lang=en');
-    httpRequest.send();
+    $.ajax({
+        url: "Home/Find",
+        type: "Get",
+        data: { search: "cats" },
+        success: function (data) { //On Success, Return Images into a grid 
+            data.data.forEach(function (item) {
+                $('#test').append(`<img src="${item.images.fixed_height.url}">`);
+            })
+        }
+    });
 }
 
-function woop() {
-    if (httpRequest.readyState == XMLHttpRequest.DONE) {
-        if (httpRequest.status === 200) {
-            console.log("I did it! Here it is!");
-            console.log(httpRequest.responseText);
-        }
-        else {
-            console.long("I failed again, sorry. I just can't do anything write anymore.");
-        }
-    }
-}
 
